7a086e012e0c8b5e534fc5eb93fd49b697f98897
SauceChord <58859889+SauceChord@users.noreply.github.com>
2023-04-12 07:00:15 +0200

    Test cleanup

--- src/io/BitWriter_spec.lua
+++ src/io/BitWriter_spec.lua
@@ -9,7 +9,7 @@ say:set("assertion.is_binary.positive", "Expected binary %s to equal %s")     --
 say:set("assertion.is_binary.negative", "Expected binary %s to not equal %s") -- Wish I could format the second string to a binary string
 assert:register("assertion", "is_binary", is_binary, "assertion.is_binary.positive", "assertion.is_binary.negative")
 
-describe("BitWriter", function()
+describe("BitWriter's function", function()
     local BitWriter
 
     setup(function()
@@ -20,22 +20,22 @@ describe("BitWriter", function()
         BitWriter = nil
     end)
 
-    it("should have a module", function()
-        assert.is_not.same(nil, BitWriter)
+    describe("new", function()
+        it("should allocate zero bytes on new", function()
+            local writer = BitWriter.new()
+
+            assert.is.same(0, #writer.bytes)
+        end)
+
+        it("should have zero bits", function()
+            local writer = BitWriter.new()
+
+            assert.is.same(0, writer.bits)
+        end)
     end)
 
-    it("should create a new instance", function()
-        assert.is_not.same(nil, BitWriter.new())
-    end)
-
-    it("should allocate zero bytes on new", function()
-        local writer = BitWriter.new()
-
-        assert.is.same(0, #writer.bytes)
-    end)
-
-    describe("writeBits", function()
-        it("should allocate one byte on writeBits(0, 8)", function()
+    describe("writeBits normal operations", function()
+        it("should allocate one byte on writeBits(_, 8)", function()
             local writer = BitWriter.new()
 
             writer:writeBits(0, 8)
@@ -44,17 +44,17 @@ describe("BitWriter", function()
             assert.is.same(1, #writer.bytes)
         end)
 
-        it("should store one byte value on writeBits(255, 8)", function()
+        it("should store one byte value", function()
             local writer = BitWriter.new()
 
-            writer:writeBits(255, 8)
+            writer:writeBits(tonumber("11111111", 2), 8)
 
             assert.is.same(8, writer.bits)
             assert.is.same(1, #writer.bytes)
             assert.is_binary("11111111", writer.bytes[1])
         end)
 
-        it("should write two bits", function()
+        it("should write two bits stored in one byte", function()
             local writer = BitWriter.new()
 
             writer:writeBits(1, 1)
@@ -65,19 +65,18 @@ describe("BitWriter", function()
             assert.is_binary("11", writer.bytes[1])
         end)
 
-        it("should write nine bits", function()
+        it("should write nine bits correctly", function()
             local writer = BitWriter.new()
 
-            writer:writeBits(1, 1)
-            writer:writeBits(tonumber("10011010", 2), 8)
+            writer:writeBits(tonumber("100110101", 2), 9)
 
             assert.is.same(9, writer.bits)
             assert.is.same(2, #writer.bytes)
             assert.is_binary("00110101", writer.bytes[1])
-            assert.is_binary("00000001", writer.bytes[2])
+            assert.is_binary("1", writer.bytes[2])
         end)
 
-        it("should write 32 bits", function()
+        it("should write 32 bits correctly", function()
             local writer = BitWriter.new()
 
             writer:writeBits(tonumber("11110000000011111111111100000000", 2), 32)
@@ -90,7 +89,7 @@ describe("BitWriter", function()
             assert.is_binary("11110000", writer.bytes[4])
         end)
 
-        it("should write 64 bits", function()
+        it("should write 64 bits correctly", function()
             local writer = BitWriter.new()
 
             writer:writeBits(tonumber("1111000000001111111111110000000011110000000011111111111100000000", 2), 64)
@@ -132,14 +131,16 @@ describe("BitWriter", function()
             assert.is.same(0, writer.bits)
             assert.is.same(0, #writer.bytes)
         end)
+    end)
 
-        it("should not write over 64 bits", function()
+    describe("writeBits error handling", function()
+        it("should not accept more than 64 bits at a time", function()
             local writer = BitWriter.new()
 
             assert.has.errors(function() writer:writeBits(0, 65) end)
         end)
 
-        it("should not write -1 bits", function()
+        it("should not accept negative number of bits", function()
             local writer = BitWriter.new()
 
             assert.has.errors(function() writer:writeBits(0, -1) end)
@@ -177,7 +178,7 @@ describe("BitWriter", function()
             assert.is.same(1, actual:byte(9))
         end)
 
-        it("two bits generates 8+1=9 bytes", function()
+        it("three bits generates 8+1=9 bytes", function()
             local writer = BitWriter.new()
             writer:writeBits(6, 3)
 
