89757bbe097d00b59e5a363509ee346727e1fff8
SauceChord <58859889+SauceChord@users.noreply.github.com>
2023-04-14 21:10:38 +0200

    Add B128 decoder

--- src/encoding/B128.lua
+++ src/encoding/B128.lua
@@ -1,33 +1,49 @@
 ---@class B128 Binary Base128 encoder/decoder
 local B128 = {}
 
----Encodes the string to its binary base 128 string.
+---Encodes a string to its binary base 128 string.
 ---The resulting encoded string contain characters whole byte is 0 to 127.
 ---@param s string String to encode to base 128.
 ---@return string encoded Base 128 encoded string.
 function B128.encode(s)
-    local sLen = #s
-    if sLen == 0 then return "" end
-    local b256 = table.pack(s:byte(1, #s))
-    local b128Out = {}
-    local b128OF = 0
-    local b128OFLen = 0
-    for i = 1, sLen do
+    local len = #s
+    if len == 0 then return "" end
+    local bytes, out, extra, extraLen = table.pack(s:byte(1, #s)), {}, 0, 0
+    for i = 1, len do
         local shl = (i - 1) % 7
-        local b128 = ((b256[i] & 127) << shl) | (b128OF & ~(-1 << shl))
-        b128OF = b256[i] >> (7 - shl)
-        b128OFLen = b128OFLen + 1
-        table.insert(b128Out, b128 & 127)
-        if b128OFLen == 7 then
-            table.insert(b128Out, b128OF)
-            b128OFLen = 0
-            b128OF = 0
+        local b128 = ((bytes[i] & 127) << shl) | (extra & ~(-1 << shl))
+        extra = bytes[i] >> (7 - shl)
+        extraLen = extraLen + 1
+        table.insert(out, b128 & 127)
+        if extraLen == 7 then
+            table.insert(out, extra)
+            extraLen = 0
+            extra = 0
         end
     end
-    if b128OFLen then
-        table.insert(b128Out, b128OF)
+    if extraLen > 0 then
+        table.insert(out, extra)
     end
-    return string.char(table.unpack(b128Out))
+    return string.char(table.unpack(out))
+end
+
+---Decodes a binary base 128 string to an original string previously encoded.
+---@param s string String to decode from base 128.
+---@return string decodedString
+function B128.decode(s)
+    local len = #s
+    if len == 0 then return "" end
+    local bytes, out, bits, buf = table.pack(s:byte(1, #s)), {}, 0, 0
+    for i = 1, len do
+        buf = buf | ((bytes[i] & 127) << bits)
+        bits = bits + 7
+        if bits >= 8 then
+            table.insert(out, buf & 255)
+            buf = buf >> 8
+            bits = bits - 8
+        end
+    end
+    return string.char(table.unpack(out))
 end
 
 return B128
--- src/encoding/B128_spec.lua
+++ src/encoding/B128_spec.lua
@@ -49,11 +49,74 @@ describe("B128", function()
                 -- Should encode to:
                 --   01010100 01010000 00100101 00011011 00000111 00100100 01011010 00111001
                 --   00100000 01000010 00000001 00100001 01010111 01101100 00011100 00111010
-                -- Encoded numbers: 
-                --   84, 80, 37, 27, 7, 36, 90, 57, 
-                --   32, 66, 1, 33, 87, 108, 28, 58
+                -- Encoded numbers:
+                --   84, 80, 37, 27, 7, 36, 90, 57,
+                --   32, 66, 1, 33, 87, 108, 28, 58, 0
                 local expected = string.char(84, 80, 37, 27, 7, 36, 90, 57, 32, 66, 1, 33, 87, 108, 28, 58)
                 local actual = B128.encode("This is a test")
+
+                assert.are.same(#expected, #actual, "encoded length")
+                for i = 1, #expected do
+                    assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
+                end
+            end)
+        end)
+    end)
+    describe("decode", function()
+        describe("when given valid inputs", function()
+            it("should decode an empty string", function()
+                assert.are.same("", B128.decode(""))
+            end)
+            it("should decode to 'a'", function()
+                local expected = "a"
+                local actual = B128.decode(string.char(97, 0))
+
+                assert.are.same(#expected, #actual, "decoded length")
+                for i = 1, #expected do
+                    assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
+                end
+            end)
+            it("should decode to (255, 255)", function()
+                local expected = string.char(255, 255)
+                local actual = B128.decode(string.char(127, 127, 3))
+
+                assert.are.same(#expected, #actual, "decoded length")
+                for i = 1, #expected do
+                    assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
+                end
+            end)
+            it("should decode to (255, 255, 255)", function()
+                local expected = string.char(255, 255, 255)
+                local actual = B128.decode(string.char(127, 127, 127, 7))
+
+                assert.are.same(#expected, #actual, "decoded length")
+                for i = 1, #expected do
+                    assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
+                end
+            end)
+            it("should decode to (255, 255, 255, 255, 255, 255, 255, 255)", function()
+                local expected = string.char(255, 255, 255, 255, 255, 255, 255, 255)
+                local actual = B128.decode(string.char(127, 127, 127, 127, 127, 127, 127, 127, 127, 1))
+
+                assert.are.same(#expected, #actual, "decoded length")
+                for i = 1, #expected do
+                    assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
+                end
+            end)
+            it("should decode to 'This is a test'", function()
+                -- "This is a test" in binary:
+                --   01010100 01101000 01101001 01110011 00100000 01101001 01110011
+                --   00100000 01100001 00100000 01110100 01100101 01110011 01110100
+                -- Should encode to:
+                --   01010100 01010000 00100101 00011011 00000111 00100100 01011010 00111001
+                --   00100000 01000010 00000001 00100001 01010111 01101100 00011100 00111010
+                -- Encoded numbers:
+                --   84, 80, 37, 27, 7, 36, 90, 57,
+                --   32, 66, 1, 33, 87, 108, 28, 58, 0
+                local expected = "This is a test"
+                local actual = B128.decode(string.char(84, 80, 37, 27, 7, 36, 90, 57, 32, 66, 1, 33, 87, 108, 28, 58))
+
+                assert.are.same(#expected, #actual, "decoded length")
                 for i = 1, #expected do
                     assert.are.same(expected:byte(i), actual:byte(i), "byte at " .. i)
                 end
